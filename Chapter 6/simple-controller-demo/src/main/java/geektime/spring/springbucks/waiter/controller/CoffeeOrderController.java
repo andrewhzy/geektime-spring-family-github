967f34c93a76cff3888f6c2ada895e29cf0331d6
hzy <andrew_hzy@163.com>
2022-12-04 23:32:02 +0800

    reformat

--- Chapter 6/simple-controller-demo/src/main/java/geektime/spring/springbucks/waiter/controller/CoffeeOrderController.java
+++ Chapter 6/simple-controller-demo/src/main/java/geektime/spring/springbucks/waiter/controller/CoffeeOrderController.java
@@ -8,11 +8,7 @@ import geektime.spring.springbucks.waiter.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -27,8 +23,7 @@ public class CoffeeOrderController {
     @ResponseStatus(HttpStatus.CREATED)
     public CoffeeOrder create(@RequestBody NewOrderRequest newOrder) {
         log.info("Receive new Order {}", newOrder);
-        Coffee[] coffeeList = coffeeService.getCoffeeByName(newOrder.getItems())
-                .toArray(new Coffee[] {});
+        Coffee[] coffeeList = coffeeService.getCoffeesByName(newOrder.getItems()).toArray(new Coffee[]{});
         return orderService.createOrder(newOrder.getCustomer(), coffeeList);
     }
 }
